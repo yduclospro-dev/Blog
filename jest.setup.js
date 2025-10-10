526041a45a49136aaa51e6efb89780d6527852be
yduclos <yduclos@sqli.com>
2025-10-10 14:15:17 +0200

    test: Add tests for container and presenter about registration feature

---
+++ jest.setup.js
@@ -0,0 +1,14 @@
+// jest.setup.js
+import '@testing-library/jest-dom'
+
+// Mock Next.js router
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+  })),
+  useSearchParams: jest.fn(() => ({
+    get: jest.fn(),
+  })),
+}))
