423176fddc7b0eb006e29bdcd17b2cc1d15d4a8d
yduclos <yduclos@sqli.com>
2025-10-10 11:40:56 +0200

    feat: Add CreateUserRequest type in userStore

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,4 +2,10 @@ export interface User {
     id: string;
     username: string;
     email: string;
+    password: string;
+}
+export interface CreateUserRequest {
+    username: string;
+    email: string;
+    password: string;
 }
