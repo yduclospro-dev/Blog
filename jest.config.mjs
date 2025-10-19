7418e712026ba8c3348e46dda9b0a3ce804cfe84
yduclos <yduclos@sqli.com>
2025-10-19 21:28:40 +0200

    refactor: reorganize tests and fix jest config
    - Create containers/ and presenters/ subdirectories for each feature
    - Move all test files to proper feature-based structure
    - Convert jest.config.js to jest.config.mjs (ES6 modules)
    - Fix import statement: next/jest -> next/jest.js
    - Remove duplicate test files
    - All 327 tests passing

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,4 +1,4 @@
-const nextJest = require("next/jest");
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
     dir: "./",
@@ -13,4 +13,4 @@ const customJestConfig = {
     },
 };
 
-module.exports = createJestConfig(customJestConfig);
+export default createJestConfig(customJestConfig);
